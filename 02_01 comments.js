7a145056962c4d66b5d0ab9e46b690f5dc108053
Madhukar Bhaktha <madzb.programming@gmail.com>
2023-09-05 07:33:43 +0530

    Section 2 : 1-3 videos source code added

---
+++ 02_01 comments.js
@@ -0,0 +1,10 @@
+console.log('Testing Github code push')
+console.log('This is a number : 24')
+//this is a comment for which the computer doesn't evaluate
+/*
+this
+is a 
+block 
+comment
+*/
+console.log("fishy fish")
