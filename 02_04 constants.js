fb9a23f3c105dcc00fa40dfcbdaf783ee82299bc
Madhukar Bhaktha <madzb.programming@gmail.com>
2023-09-05 08:50:56 +0530

    Section 2: Updated till 4.Constats..1 File renamed

---
+++ 02_04 constants.js
@@ -0,0 +1,6 @@
+
+// constants have only 1 value EVER
+const Madz4Const = "Some value to start with"
+console.log(Madz4Const)
+// Madz4Const = "try to modify it " ERROR : assignement to const variable
+// console.log(Madz4Const)
