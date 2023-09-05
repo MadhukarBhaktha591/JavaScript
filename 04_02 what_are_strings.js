e1b91dadd8403ec6eb3ac9a01d912831f4231989
Madhukar Bhaktha <madzb.programming@gmail.com>
2023-09-06 00:07:10 +0530

    Section 3 exercises..notes started frm section 4..

---
+++ 04_02 what_are_strings.js
@@ -0,0 +1,10 @@
+console.log('helllloooooooooooooowrld',24)
+
+//what are string?
+//strings are just a group of characters that are enclosed within '', "", or ``
+const singleQuoteString =   'this is a string using single quotes';
+const doubleQuoteString = "this is a string using double quote string";
+let backtickString = `this is a string using backticks`;
+
+//in javascript we can check whether if the two things are exactly the same or not bu using ===
+console.log('a' === "a");
