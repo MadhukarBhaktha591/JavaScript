c4443dd97b3c36828f82ee335e77c939e4a16584
Madhukar Bhaktha <madzb.programming@gmail.com>
2023-09-11 19:45:14 +0530

    Section 13 ..6 files added newly..source codes ..

---
+++ 05-ifCONDITIONS/13_08-else.js
@@ -0,0 +1,12 @@
+const test = false;
+
+if (test) {
+    console.log('the test is true');
+}
+else {
+    console.log('the test is false');
+}
+
+/*
+    else statement fire when the if conditon's test(the test inside the parentheses ) evaluates to false..
+*/
