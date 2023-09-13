b99660e05b674b4ae3f0fbb5b95b3b53c6ce09a7
Madhukar Bhaktha <madzb.programming@gmail.com>
2023-09-13 15:35:52 +0530

    CODE: Section 15 starts

---
+++ 07-LOOPS/15_00-firstLoop.js
@@ -0,0 +1,24 @@
+//loops allow us to write code that can be executed mutltiple times
+//(however many times you write)
+
+//printing from 1 to 10
+for (let count = 0; count < 10; count += 1) {
+    console.log(`Hello ${count + 1}`);
+}
+
+//printing from 1 to 17
+for (let count = 0; count < 17; count += 1) {
+    console.log(`Hello ${count + 1}`);
+}
+
+//printing from 8 to 17
+for (let count = 7; count < 17; count += 1) {
+    console.log(`Hello ${count + 1}`);
+
+}
+
+//printing goodbye from 8 to 17...in increments of 3
+
+for (let count = 7; count < 17; count += 3) {
+    console.log(`Goodbye ${count + 1}`);
+}
