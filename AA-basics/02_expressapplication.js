d1bca12c4cb020b0fdc191705746f81cae7b3196
MadhukarBhaktha591 <madzbprogramming@gmail.com>
2024-06-25 22:40:44 +0530

    Update 02_expressapplication.js

--- AA-basics/02_expressapplication.js
+++ AA-basics/02_expressapplication.js
@@ -10,3 +10,29 @@ const port = 3000;
 app.listen(port,()=>{
     console.log(`Server running on http://localhost:${port}`)
 })
+
+
+
+/** Dummy data */
+/** */
+let blogs = [
+]    
+;
+
+//Create a new blog
+app.post('/blog',(req,res)=> {
+        console.log(req.body);
+    blogs.push(req.body);    
+res.send(blogs)    
+    //res.status(201).json(blogs);
+});    
+
+
+
+//Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//express.urlencoded({ extended: true});
+//express.json();
+
